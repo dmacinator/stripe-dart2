4a0340431d8af5c0e44192980e1077c5c1a67e44
dmacinator <121703414+dmacinator@users.noreply.github.com>
2024-02-20 14:08:09 -0500

    m

--- lib/src/resources/ephemeral_keys.dart
+++ lib/src/resources/ephemeral_keys.dart
@@ -14,7 +14,7 @@ class EphemeralKeysResource extends Resource<EphemeralKeys> {
   }
 
   Future<EphemeralKeys> delete(String keyId) async {
-    final map = await del('ephmeral_kesys/$keyId');
+    final map = await del('ephemeral_keys/$keyId');
     return EphemeralKeys.fromJson(map);
   }
 }
